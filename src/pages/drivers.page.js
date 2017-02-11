04a0e491ec117cf15619621ccf4212a3f5fa21c7
Adam Ramski <adam.ramski@aply.eu>
2017-02-11 15:56:38 +0100

    Adds UI of Driver component

--- src/pages/drivers.page.js
+++ src/pages/drivers.page.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React  from 'react'
+import Driver from '../components/Driver'
 
 export default class DriversPage extends React.Component {
   render () {
     return (
-      <div>
-        <h1>DriversPage</h1>
+      <div id='driversPage'>
+        <Driver />        
         <div id='footer' />
       </div>
     )
