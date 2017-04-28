053116a5fc3fcc95f5f71e0a54ad4d94b54ffe6d
adam.ramski <adam.ramski@aply.eu>
2017-04-28 09:00:31 +0200

    Update previous month drivers dialog

--- src/views/PreviousMonthDrivers.js
+++ src/views/PreviousMonthDrivers.js
@@ -15,19 +15,21 @@ export default class PreviousMonthDrivers extends React.Component {
         }}>
           <form
             className='driver-details'
-            style={{height: 'initial', width: 'initial'}}>
+            style={{height: 600, width: 350}}>
             <h3>
               Zaznacz którzy kierowcy mieli nocki w ostatnim
               dniu poprzedniego miesiąca:
             </h3>
-            {
-              this.props.drivers.map((driver) =>
-                <Checkbox
-                  key={driver.id}
-                  label={utils.padNumber(driver.id).toString()}
-                  handleCheckboxChange={AppActions.togglePreviousMonthDriver} />
-              )
-            }
+            <div style={{height: 413, overflowY: 'auto'}}>
+              {
+                this.props.drivers.map((driver) =>
+                  <Checkbox
+                    key={driver.id}
+                    label={utils.padNumber(driver.id).toString()}
+                    handleCheckboxChange={AppActions.togglePreviousMonthDriver} />
+                )
+              }
+            </div>
             <div className='centered-div' style={{marginTop: '20px'}}>
               <input
                 type='button'
