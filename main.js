5b3664b00e4428a7c938c494ed74055fbdaa8548
adam.ramski <adam.ramski@aply.eu>
2017-05-31 22:22:59 +0200

    Delete design assets and fix typo

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ function createWindow () {
 app.on('ready', createWindow)
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quite()
+    app.quit()
   }
 })
 app.on('activate', () => {
